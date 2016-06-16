de3dd0e9b883a5a1e80fd9382eadf31f511f5e3e
Chyld Medford <chyld.medford@gmail.com>
2016-06-16 10:01:46 -0700

    accounts

--- test/account.spec.js
+++ test/account.spec.js
@@ -1,15 +1,44 @@
 const expect = require('chai').expect;
 const Account = require('../lib/account');
 
-describe('Account', function(){
-  describe('constructor', function(){
-    it('constructs a new Account object', function(){
-      const a1 = new Account('Sara', 500);
+describe('Account', () => {
+  describe('constructor', () => {
+    it('constructs a new Account object', () => {
+      const a1 = new Account('Sara');
       expect(a1).to.be.instanceof(Account);
       expect(a1.number).to.be.length(36);
       expect(a1.name).to.equal('Sara');
-      expect(a1.balance).to.equal(500);
+      expect(a1.balance).to.equal(0);
       expect(a1.deposits).to.be.length(0);
+      expect(a1.withdraws).to.be.length(0);
+    });
+  });
+
+  describe('#deposit', () => {
+    it('deposits money into account', () => {
+      const a1 = new Account('Sara');
+      const bal = a1.deposit(50);
+      expect(bal).to.equal(50);
+      expect(a1.balance).to.equal(50);
+      expect(a1.deposits).to.be.length(1);
+    });
+  });
+
+  describe('#withdraw', () => {
+    it('successfully withdraws from account', () => {
+      const a1 = new Account('Sara');
+      a1.deposit(50);
+      const bal = a1.withdraw(25);
+      expect(bal).to.equal(25);
+      expect(a1.balance).to.equal(25);
+      expect(a1.withdraws).to.be.length(1);
+    });
+    it('does not successfully withdraws from account', () => {
+      const a1 = new Account('Sara');
+      a1.deposit(50);
+      const bal = a1.withdraw(100);
+      expect(bal).to.equal(50);
+      expect(a1.balance).to.equal(50);
       expect(a1.withdraws).to.be.length(0);
     });
   });
